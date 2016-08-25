6f27b669ef66877808fe633d1060926ac722d83e
Romain Veltz - Hebb <romain.veltz@inria.fr>
2016-08-25 12:00:45 +0200

    correct mistake in examples

--- examples/morris_lecar.jl
+++ examples/morris_lecar.jl
@@ -48,15 +48,17 @@ xd0 = vec([Int(p0["N"]),    #Na closed
 
 nu_ml = [[-1 1 0 0];[1 -1 0 1];[0 0 -1 1];[0 0 1 -1]]
 parms = vec([0.])
-tf = p1["t_end"]
-tf=350.
+tf = p1["t_end"];tf=350.
 dummy_t = PDMP.chv(6,xc0,xd0, F_ml, R_ml,(x,y,t,pr,id)->vec([0.]), nu_ml , parms,0.0,0.01,false)
+
 srand(123)
 dummy_t = @time PDMP.chv(4500,xc0,xd0, F_ml, R_ml,(x,y,t,pr,id)->vec([0.]), nu_ml , parms,0.0,tf,false)
 result =  PDMP.chv_optim(2,xc0,xd0,F_type_ml,R_type_ml,DX_type_ml,nu_ml,parms,0.0,tf,false)
+
 srand(123)
 result =  @time PDMP.chv_optim(4500,xc0,xd0,F_type_ml,R_type_ml,DX_type_ml,nu_ml,parms,0.0,tf,false) #cpp= 100ms/2200 jumps
 println("#jumps = ", length(dummy_t.time)," ", length(result.time))
+
 try
   println(norm(dummy_t.time-result.time))
   println("--> xc_f-xc_t = ",norm(dummy_t.xc-result.xc))
