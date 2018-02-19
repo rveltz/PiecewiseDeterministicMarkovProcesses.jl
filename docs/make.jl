c515bcedab7e8a0302c415d4930fa247e112f752
Romain Veltz - Hebb <romain.veltz@inria.fr>
2018-02-19 11:17:53 +0100

    improve docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,12 +2,11 @@ using Documenter, PDMP
 
 makedocs(
 	format = :html,
-	sitename = "Piecewise Deterministic Markov Processes in Julia ",
-	pages = ["Home" => "index.md"]
+	sitename = "Piecewise Deterministic Markov Processes in Julia "
 )
 
-ENV["DOCUMENTER_DEBUG"] = true
-ENV["TRAVIS_REPO_SLUG"] = "github.com/rveltz/PDMP.jl.git"
+# ENV["DOCUMENTER_DEBUG"] = true
+# ENV["TRAVIS_REPO_SLUG"] = "github.com/rveltz/PDMP.jl.git"
 
 deploydocs(
 	deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-cinder"),
