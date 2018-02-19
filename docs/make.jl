c5c44f4a9c3d01b29bb361843a77aa22acb7851f
Romain Veltz - Hebb <romain.veltz@inria.fr>
2018-02-19 08:00:07 +0100

    improve docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, PDMP
 makedocs()
 
 deploydocs(
-	deps   = Deps.pip("mkdocs", "python-markdown-math"),
+	deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-cinder"),
     repo   = "github.com/rveltz/PDMP.jl.git",
 	julia  = "0.6",
 	osname = "linux"
