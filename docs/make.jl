f157951a97d2a97822ae2ea97eb7ebb96e9bf7cd
Romain Veltz - Hebb <romain.veltz@inria.fr>
2018-02-19 12:25:12 +0100

    improve docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,6 @@
 using Documenter, PDMP
 
-makedocs(
-	format = :html,
-	sitename = "Piecewise Deterministic Markov Processes in Julia "
-)
+makedocs()
 
 # ENV["DOCUMENTER_DEBUG"] = true
 # ENV["TRAVIS_REPO_SLUG"] = "github.com/rveltz/PDMP.jl.git"
