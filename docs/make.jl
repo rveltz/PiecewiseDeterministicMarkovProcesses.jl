665b2756ec5a9a8f992acb59fabea02f5ad5bb73
Romain Veltz <rveltz@salk.edu>
2018-12-19 08:14:14 +0100

    docs change theme

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,22 @@
 using Documenter, PiecewiseDeterministicMarkovProcesses
 
-makedocs(
-	# format = :html,
-	sitename = "Piecewise Deterministic Markov Processes in Julia "
-	)
+makedocs(doctest = true,
+			clean = true,
+			format = :html,
+			sitename = "PiecewiseDeterministicMarkovProcesses.jl"
+			)
 
 # ENV["DOCUMENTER_DEBUG"] = true
 # ENV["TRAVIS_REPO_SLUG"] = "github.com/rveltz/PiecewiseDeterministicMarkovProcesses.jl.git"
 
 deploydocs(
-	deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-cinder", "pygments"),
+	# deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-cinder", "pygments"),
 	repo   = "github.com/rveltz/PiecewiseDeterministicMarkovProcesses.jl.git",
 	julia  = "1.0",
 	osname = "linux",
+    target = "build",
+    deps   = nothing,
+	make = nothing
 )
+
 
