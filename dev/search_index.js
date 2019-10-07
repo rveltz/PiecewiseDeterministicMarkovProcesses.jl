711dbb7352434a55d29ea644b8940ebee5c28608
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-07 07:57:45 +0000

    build based on 3e1a1e6

--- dev/search_index.js
+++ dev/search_index.js
@@ -170,18 +170,26 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "addFeatures/#",
-    "page": "Additional Features",
-    "title": "Additional Features",
+    "page": "FAQ",
+    "title": "FAQ",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "addFeatures/#Specify-a-jump-with-a-function-1",
-    "page": "Additional Features",
+    "page": "FAQ",
     "title": "Specify a jump with a function",
     "category": "section",
     "text": "See examples/pdmp_example_eva.jl for an example."
+},
+
+{
+    "location": "addFeatures/#Rejection-method-stopped,-recover-data!-1",
+    "page": "FAQ",
+    "title": "Rejection method stopped, recover data!",
+    "category": "section",
+    "text": "If you chose an upper bound for the rejection method that is too small and triggers an interruption likeERROR: AssertionError: Error, your bound on the rates is not high enough!, [26.730756983739408, 20.0]the solve does not return anything. However, in order to understand why your bound is too small, you would like to have a look at your trajectory up to the point where you bound failed. Don\'t worry, your computation is still in memory!If your call is like this:sol = solve(problem, Rejection(Tsit5()) )then the trajectory is saved in the variables problem.time, problem.Xc and problem.Xd."
 },
 
 ]}
